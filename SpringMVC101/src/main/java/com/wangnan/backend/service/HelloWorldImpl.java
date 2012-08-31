7269a040bd3e4b092618712b511658de9a05d4e5
wangnan <superwangnan@gmail.com>
2012-08-31 23:28:34 +0800

    something wrong with hibernate configuration. remove the hibernate-annoation-common component. and if we use the hibernate 4, we need change the sessionfactory and transaction manager class in the root-context config, otherwise there is class not found error. I think the hibernate 4 remove some classes from jar.
    add the code to get session factory from servlet context.
    
    So far, the project can create the table in db automatically.
    
    congratulations!!!!

--- SpringMVC101/src/main/java/com/wangnan/backend/service/HelloWorldImpl.java
+++ SpringMVC101/src/main/java/com/wangnan/backend/service/HelloWorldImpl.java
@@ -1,12 +1,38 @@
 package com.wangnan.backend.service;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import org.apache.cxf.transport.http.AbstractHTTPDestination;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @WebService(endpointInterface = "com.wangnan.backend.service.HelloWorld")
 public class HelloWorldImpl implements HelloWorld {
-
+	@Resource
+	private WebServiceContext context;
     public String sayHi(String text) {
         System.out.println("sayHi called");
+        
+        MessageContext ctx = context.getMessageContext();
+        HttpServletRequest request = (HttpServletRequest) 
+            ctx.get(AbstractHTTPDestination.HTTP_REQUEST);
+        HttpServletResponse response = (HttpServletResponse) 
+            ctx.get(AbstractHTTPDestination.HTTP_RESPONSE);
+        
+        ApplicationContext beanFactory =
+        	    WebApplicationContextUtils
+        	        .getRequiredWebApplicationContext(request.getSession().getServletContext());
+        
+        SessionFactory sf = (SessionFactory) beanFactory.getBean("sessionFactory");
+        Session session=sf.openSession();
+        session.close();
         return "Hello " + text;
     }
 }
